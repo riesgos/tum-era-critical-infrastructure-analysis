f504d9361fb9461156f3f5f076bfa5ed279517bd
HugoRosero <hugo.rosero@tum.de>
2020-02-19 16:58:59 +0100

    Forced to extract a[0] for oneIntensity case

--- System_Reliability_Analysis_local.py
+++ System_Reliability_Analysis_local.py
@@ -166,10 +166,11 @@ def main():
         NetworkFragility=import_json_to_dict(fragility_files[0])# sources and terminals do not depend on the intensity measure
     else: #hazard with one single intensity measure
         fragility_file = os.path.join(folder_prefix, fragility_file_prefix + '_NetworkFragility.json')
-        try:
-            evaluate_ProbFailure_oneIntensity(fragility_file,im_file_list,DamageNodes)
-        except:
-            evaluate_ProbFailure_oneIntensity(fragility_file,im_file_list[0],DamageNodes)
+        #try:
+            #evaluate_ProbFailure_oneIntensity(fragility_file,im_file_list,DamageNodes)
+        #except:
+            #evaluate_ProbFailure_oneIntensity(fragility_file,im_file_list[0],DamageNodes)
+        evaluate_ProbFailure_oneIntensity(fragility_file,im_file_list[0],DamageNodes)
         NetworkFragility=import_json_to_dict(fragility_file)
     
     
